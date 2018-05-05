3352d5d1de7043e9918552c9614ed4910ed3af03
mrnexeon <the.nexeon@gmail.com>
2018-05-05 19:36:41 +0300

    Demo website

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode: 'development',
     entry: './src/client/index.js',
     output: {
-        path: path.resolve(__dirname, "public"),
+        path: path.resolve(__dirname, "public/build"),
         filename: "bundle.js"
     },
     module: {
