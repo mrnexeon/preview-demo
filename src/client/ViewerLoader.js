370d3340a6f76886ffe7c353cbfd152bee2720fb
Ivan Dashkevich <idashkevich@lement.pro>
2018-05-16 13:10:30 +0300

    Sit on place on selection with corrent camera

--- src/client/ViewerLoader.js
+++ src/client/ViewerLoader.js
@@ -1,8 +1,8 @@
-const urn = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTA0LTA2LTE5LTI3LTEyLWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlL2xhX21hcm1pdGVfXzEuc2tw';
+const urn = 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6bW9kZWwyMDE4LTA1LTA1LTE3LTUxLTA4LWQ0MWQ4Y2Q5OGYwMGIyMDRlOTgwMDk5OGVjZjg0MjdlLzEyMy5za3A';
 const initialViewableIndex = 0;
 
 var viewerApp;
-var viewerObserver;
+var viewer;
 
 function load(token) {
     var options = {
@@ -29,16 +29,51 @@ function onDocumentLoadFailure(viewerErrorCode) {
     throw ('onDocumentLoadFailure() - errorCode:' + viewerErrorCode);
 }
 
-function onEverythingLoaded(e) {
-    viewerObserver.publish('VIEWER_TEXTURES_LOADED', e);
-}
-
 function onViewableLoadSuccess(viewer, viewable) {
-    viewerObserver.publish('VIEWER_LOADED', viewer);
+    viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, function(){  
+        var forgeId = viewer.getSelection();
+        sitOnPlace(viewer, forgeId);
+    })
 }
 
 function onViewableLoadFail(errorCode) {
     throw ('onItemLoadFail() - errorCode:' + errorCode);
+}
+
+function sitOnPlace(viewer, forgeId) {
+    var item = viewer.impl.model.getData().fragments.fragId2dbId.indexOf(parseInt(forgeId));
+    if (item == -1) return;
+
+    var fragbBox = new THREE.Box3();
+    var nodebBox = new THREE.Box3();
+
+    [item].forEach(function (fragId) {
+        viewer.model.getFragmentList().getWorldBounds(fragId, fragbBox);
+        nodebBox.union(fragbBox);
+    });
+
+    var bBox = nodebBox;
+
+    var camera = viewer.getCamera();
+    var navTool = new Autodesk.Viewing.Navigation(camera);
+    navTool.toPerspective();
+
+    var position = bBox.max;
+    position.x -= 0.5;
+    position.y -= 0.25;
+
+    var pivPointPosition = JSON.parse(JSON.stringify(position));
+    pivPointPosition.z -= 0.1
+    navTool.setPivotPoint(pivPointPosition);
+    navTool.setPivotSetFlag(true);
+    viewer.setUsePivotAlways(true);
+    navTool.setVerticalFov(70, true);
+
+    var target = new THREE.Vector3(-7, 2, -10.5);
+    var up = new THREE.Vector3(0, 0, 1);
+
+    navTool.setView(position, target);
+    navTool.setWorldUpVector(up, true);
 }
 
 export {load}
