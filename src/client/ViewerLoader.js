7dfc375f5049aa3099f5cdc6b850572c73d82af0
Ivan Dashkevich <idashkevich@lement.pro>
2018-05-19 16:56:36 +0300

    Cleanup 2

--- src/client/ViewerLoader.js
+++ src/client/ViewerLoader.js
@@ -31,7 +31,7 @@ function onDocumentLoadFailure(viewerErrorCode) {
 }
 
 function onViewableLoadSuccess(viewer, viewable) {
-    if (URLPlace)
+    if (!!URLPlace)
     {
         sitOnPlace(viewer, URLPlace);
     } else {
